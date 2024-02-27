6f6270d665de5f9d7597654152d7beb2cbc0a4e6
najmils <najmilhayh6@gmail.com>
2024-02-27 15:27:47 +0700

    fix bugs

--- app/Views/daftaripp/detail.php
+++ app/Views/daftaripp/detail.php
@@ -161,7 +161,7 @@
                             <a href="<?= base_url('daftaripp/index') ?>" class="btn btn-primary mr-2 btn-sm" style="width: 100px; height: 30px;">Back</a>
                         <?php } ?>
                         <?php
-                        // dd($isWithinIPPeriode && $is_approved_before);
+                        // dd(session()->get('npk') != 0 && $editIppMid && $is_approved_before && $is_approved);
                             if (session()->get('npk') != 0 && $isWithinIPPeriode && $is_approved_before && $is_approved) {
                                 foreach ($ippmain as $p){
                                     // Approval Kasie
@@ -188,7 +188,7 @@
                                         }
 
                                         if ($p['kode_jabatan'] == 4 || ($p['kode_jabatan'] == 8 && $p['created_by'] == [3651, 3659])) {
-                                            if (session()->get('kode_jabatan') == 3 && empty($p['approval_kadept'])) {
+                                            if (empty($p['approval_kadept'])) {
                                                 echo '<a href="' . base_url("/daftaripp/approveKadept/{$p['id']}") . '" class="approve-button btn btn-success btn-sm mr-2" style="width: 100px; height: 30px;">
                                                     <i class="fas fa-check" style="color: white;">Approve</i>
                                                 </a>';
